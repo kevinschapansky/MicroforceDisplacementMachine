e2b315de51b09e8ae2b47bc2086abcaaeb71da33
Kevin Schapansky <kschapansky.dev@gmail.com>
2013-03-01 00:10:22 -0800

    CreatedVSProject
    Stage serial control classes added

---
+++ MFDMInterface/MFDMInterface/StageSerialCom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.IO;
+
+namespace MFDMInterface
+{
+    class StageSerialCom
+    {
+        static const int BAUD = 19200;
+
+        SerialPort StagePort;
+
+        public StageSerialCom(string comPort)
+        {
+            StagePort = new SerialPort(comPort);
+            StagePort.BaudRate = BAUD;
+            //StagePort.Parity = 
+        }
+    }
+}
